5e0af1b2cc747f68281f4c595eec21b3d4e8816b
Alex.tang <173788951@qq.com>
2021-02-03 18:12:42 +0800

    2021/02/03 18:28:00 commit

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__,)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world！'
+    return 'Hello world'
 
 @app.route('/home/')
 def home():
     return 'Welecome to home!'
 
+#输入整数
 @app.route('/home/<int:num>')
 def int(num):
     #return '数字是：{}'.format(num)
     return f'数字是：{num}'
 
+#输入浮点数
 @app.route('/home/<float:num>')
 def float(num):
     return f'浮点数是：{num}'
     #return '浮点数是：{}'.format(num)
 
+#输入字符串
 @app.route('/home/<name>')
 def str(name):
     return f'名字是: {name}'
     #return '名字是:{}'.format(name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=8800,debug=True)
 
